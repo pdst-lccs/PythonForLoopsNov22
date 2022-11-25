fcfd00b592bc8a46bfdbaeb066dcec92b24114d4
pdst-lccs <50370310+pdst-lccs@users.noreply.github.com>
2022-11-25 08:27:23 +0000

    Update MMTv1.py

--- MMTv1.py
+++ MMTv1.py
@@ -11,7 +11,7 @@ n1 = random.randint(0, 12) # Generate the 1st number
 n2 = random.randint(0, 12) # Generate the 2nd number
 ans = n1*n2 # Calculate the product and store it in 'ans'
 
-print("%d * %d" %(n1,n2))  # Display the expression
+print(n1, "*", n2)       # Display the expression
 usrAns = int(input("Enter your answer: ")) # Read the response
 
 # Evaluate the condition
